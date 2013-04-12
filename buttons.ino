6ed40549ce9c378bbf13b8e4b4e959665f601003
Yurij Mikhalevich <0@39.yt>
2013-04-12 19:40:13 +0400

    Code refactored. False start button functionality properly implemented.

--- buttons.ino
+++ buttons.ino
@@ -1,6 +1,6 @@
 #include <MsTimer2.h>
 
-#define PULSE_WIDTH_USEC 5                   // Импульс "загрузка/чтение" для 74HC165. 
+#define PULSE_WIDTH_USEC 5                   // Импульс "загрузка/чтение" для 74HC165.
 
 const int casc_ds = 2; //data pin
 const int casc_st_cp =  3; //latch pin
@@ -16,52 +16,59 @@ const int tmblr = 12;
 //кнопки левые  : синяя - 4 (7); зелёная - 5 (5); жёлтая - 6 (3); красная - 7 (1).
 //кнопки правые : синяя - 3 (8); зелёная - 2 (6); жёлтая - 1 (4); красная - 0 (2).
 
-int buttons[8] = {0, 1, 2, 3, 4, 5, 6, 7};
-int digits[8] = {2, 4, 6, 8, 7, 5, 3, 1};
+const int buttons[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+const int digits[8] = {2, 4, 6, 8, 7, 5, 3, 1};
 
 boolean casc_state[24] = {0, 0, 0, 0, 0, 0,
                           0, 0, 0, 0, 0, 0,
                           0, 0, 0, 0, 0, 0, 
                           0, 0, 0, 0, 0, 0};
-boolean numbers[23][8] = {{0, 0, 0, 0, 0, 0, 1, 1},
-                          {1, 0, 0, 1, 1, 1, 1, 1},
-                          {0, 0, 1, 0, 0, 1, 0, 1},
-                          {0, 0, 0, 0, 1, 1, 0, 1},
-                          {1, 0, 0, 1, 1, 0, 0, 1},
-                          {0, 1, 0, 0, 1, 0, 0, 1},
-                          {0, 1, 0, 0, 0, 0, 0, 1},
-                          {0, 0, 0, 1, 1, 1, 1, 1},
-                          {0, 0, 0, 0, 0, 0, 0, 1},
-                          {0, 0, 0, 0, 1, 0, 0, 1},
-                          {0, 0, 0, 0, 0, 0, 1, 0},
-                          {1, 0, 0, 1, 1, 1, 1, 0},
-                          {0, 0, 1, 0, 0, 1, 0, 0},
-                          {0, 0, 0, 0, 1, 1, 0, 0},
-                          {1, 0, 0, 1, 1, 0, 0, 0},
-                          {0, 1, 0, 0, 1, 0, 0, 0},
-                          {0, 1, 0, 0, 0, 0, 0, 0},
-                          {0, 0, 0, 1, 1, 1, 1, 0},
-                          {0, 0, 0, 0, 0, 0, 0, 0},
-                          {0, 0, 0, 0, 1, 0, 0, 0},
-                          {1, 1, 1, 1, 1, 1, 1, 1},
-                          {1, 1, 1, 1, 1, 1, 1, 0},
-                          {1, 1, 1, 1, 1, 0, 1, 1}};
+const boolean numbers[23][8] = {{0, 0, 0, 0, 0, 0, 1, 1},
+                                {1, 0, 0, 1, 1, 1, 1, 1},
+                                {0, 0, 1, 0, 0, 1, 0, 1},
+                                {0, 0, 0, 0, 1, 1, 0, 1},
+                                {1, 0, 0, 1, 1, 0, 0, 1},
+                                {0, 1, 0, 0, 1, 0, 0, 1},
+                                {0, 1, 0, 0, 0, 0, 0, 1},
+                                {0, 0, 0, 1, 1, 1, 1, 1},
+                                {0, 0, 0, 0, 0, 0, 0, 1},
+                                {0, 0, 0, 0, 1, 0, 0, 1},
+                                {0, 0, 0, 0, 0, 0, 1, 0},
+                                {1, 0, 0, 1, 1, 1, 1, 0},
+                                {0, 0, 1, 0, 0, 1, 0, 0},
+                                {0, 0, 0, 0, 1, 1, 0, 0},
+                                {1, 0, 0, 1, 1, 0, 0, 0},
+                                {0, 1, 0, 0, 1, 0, 0, 0},
+                                {0, 1, 0, 0, 0, 0, 0, 0},
+                                {0, 0, 0, 1, 1, 1, 1, 0},
+                                {0, 0, 0, 0, 0, 0, 0, 0},
+                                {0, 0, 0, 0, 1, 0, 0, 0},
+                                {1, 1, 1, 1, 1, 1, 1, 1},
+                                {1, 1, 1, 1, 1, 1, 1, 0},
+                                {1, 1, 1, 1, 1, 0, 1, 1}};
                           
 int sounds[8] = {60, 80, 100, 120, 140, 160, 180, 200};
-int order[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+struct pressed_button {
+  short button_id;
+  short cycles_beeped;
+};
+pressed_button *order = new pressed_button[8];
 int beep_pointer = 0;
-int beeped = 0;
+
+#define BEEP_CYCLES 20
+
+struct button_state {
+  boolean state;
+  boolean false_start;
+  short cycles_away;
+};
+
+button_state *buttons_state = new button_state[8];
+
+#define MAX_INACTIVE_CYCLES 2
 
 boolean just_one_pressed; //флаг, показывающий, нажата ли хотя бы одна кнопка, важно для работы тумблера
-
-/*void write_casc_state();
-void set_casc_state(boolean state = 0);
-int pow(int number, int exponent); //because internal does not work properly
-void display_number(int at, int number); /*at should be in [1:8] and number in [0:21],
-                                          where 0:9 is numbers without dot and 10-19 - with,
-                                          20 completely switches of led and 21 blip only dot.*/
-/*int readButtonsState(int j);
-void beep();*/
 
 void setup() {
   Serial.begin(9600);
@@ -78,7 +85,7 @@ void setup() {
   digitalWrite(btn_clk, HIGH);
   digitalWrite(btn_sh_ld, HIGH);
   resetApp();
-  MsTimer2::set(100, mloop);
+  MsTimer2::set(25, mloop);
   MsTimer2::start();
 }
 
@@ -86,9 +93,13 @@ void resetApp() {
   analogWrite(buzzer, 0);
   for (int i = 0; i < 8; ++i) {
     display_number(i + 1, 21);
-    order[i] = 0;
+    order[i].button_id = 0;
+    order[i].cycles_beeped = 0;
+    beep_pointer = 0;
+    buttons_state[i].state = false;
+    buttons_state[i].false_start = false;
+    buttons_state[i].cycles_away = 0;
   }
-  Serial.write("Hello, reset!\n");
   just_one_pressed = false;
 }
 
@@ -98,52 +109,66 @@ void loop() {
 //главный цикл
 
 void mloop() {
-  boolean b = false;
-  if (digitalRead(fstart) == HIGH) {
-    return;
-  } else if (digitalRead(reset) == HIGH) {
+  beep();
+  if (digitalRead(reset) == HIGH) {
     resetApp();
+    return;
   }
   if (digitalRead(tmblr) == HIGH && just_one_pressed) {
     return;
   }
+  readButtonsState();
   for (int i = 0; i < 8; ++i) {
-    if (readButtonsState(i) == HIGH) {
-      buttonPressed(digits[i]);
-      analogWrite(buzzer, 200);
-      b = true;
-      just_one_pressed = true;
-      if (digitalRead(tmblr) == HIGH) {
-        return;
+    if (buttons_state[i].state) {
+      if (digitalRead(fstart) == HIGH) {
+        buttons_state[i].false_start = true;
+      } else {
+        if (buttons_state[i].false_start) {
+          continue;
+        }
+        buttonPressed(digits[i]);
+        just_one_pressed = true;
+        if (digitalRead(tmblr) == HIGH) {
+          return;
+        }
       }
     }
-  }
-  if (!b) {
-    analogWrite(buzzer, 0);
   }
 }
 
 // конец
 
-int readButtonsState(int j) {
-  int state[8];
+void readButtonsState() {
   digitalWrite(btn_sh_ld, LOW);
   digitalWrite(btn_sh_ld, HIGH);
+  int bstate;
   for (int i = 0; i < 8; ++i) {
-    state[i] = digitalRead(btn_qh);
-//    Serial.write(state[i]);
+    bstate = digitalRead(btn_qh);
+    if (bstate == LOW) {
+      if (buttons_state[i].state) {
+        if (buttons_state[i].cycles_away == MAX_INACTIVE_CYCLES) {
+          buttons_state[i].state = false;
+          buttons_state[i].false_start = false;
+          buttons_state[i].cycles_away = 0;
+        } else {
+          ++buttons_state[i].cycles_away;
+        }
+      }
+    } else {
+      buttons_state[i].state = true;
+      buttons_state[i].cycles_away = 0;
+    }
     digitalWrite(btn_clk, LOW);
     digitalWrite(btn_clk, HIGH);
   }
-  return state[j];
 }
 
 void buttonPressed(int button) {
   for (int i = 0; i < 8; ++i) {
-    if (order[i] == button) {
+    if (order[i].button_id == button) {
       return;
-    } else if (order[i] == 0) {
-      order[i] = button;
+    } else if (order[i].button_id == 0) {
+      order[i].button_id = button;
       display_number(button, i + 1);
       return;
     }
@@ -151,20 +176,25 @@ void buttonPressed(int button) {
 }
 
 void beep() {
-//  MsTimer2::stop();
+  if (order[beep_pointer].cycles_beeped == BEEP_CYCLES) {
+    ++beep_pointer;
+  }
   if (beep_pointer != 8) {
-    if (order[beep_pointer] != 0) {
-      analogWrite(buzzer, sounds[order[beep_pointer++] - 1]);
-//      MsTimer2::set(1000, beep);
-    } else {
-      analogWrite(buzzer, 0);
-//      MsTimer2::set(250, beep);
+    if (order[beep_pointer].button_id != 0) {
+      if (order[beep_pointer].cycles_beeped == 0) {
+        analogWrite(buzzer, sounds[order[beep_pointer].button_id - 1]);
+      }
+      ++order[beep_pointer].cycles_beeped;
+      return;
     }
   }
-//  MsTimer2::start();
+  analogWrite(buzzer, 0);
 }
 
 void display_number(int at, int number) {
+  /*at should be in [1:8] and number in [0:21], where 0:9 is numbers without dot and 10-19 - with,
+   *20 completely switches of led and 21 blip only dot.
+   */
   int ds = 3 * (at - 1);
   int st_cp = ds + 1;
   int sh_cp = ds + 2;
@@ -208,6 +238,7 @@ void set_casc_state(boolean state) {
 }
 
 int pow(int number, int exponent) {
+  /*because internal does not work properly or i am a dumbass*/
   int result = 1;
   for (int i = 0; i < exponent; ++i) {
     result *= number;
